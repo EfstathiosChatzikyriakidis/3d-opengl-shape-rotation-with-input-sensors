25e54c349656f5a1e84636aa7b8c1df7841a95e4
Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@gmail.com>
2022-04-25 00:22:57 +0300

    Change.

--- development/3d_opengl_shape_rotation_with_input_sensors.py
+++ development/3d_opengl_shape_rotation_with_input_sensors.py
@@ -3,7 +3,7 @@
 
 #    3D OpenGL Shape Rotation With Input Sensors.
 #
-#    Copyright (C) 2010 Efstathios Chatzikyriakidis <contact@efxa.org> 
+#    Copyright (C) 2010 Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@gmail.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
